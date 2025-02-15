795eb26075c621d8ed06f054bcd3d89753994f05
Chris Irag <irag.chrisandrei@mailbox.org>
2025-02-15 01:35:32 +0000

    docs: added inline docs

--- src/main.py
+++ src/main.py
@@ -12,15 +12,35 @@ app = FastAPI()
 
 @app.get("/data")
 def all_data():
+    """Fetch all data from the CSV file.
+
+    Returns:
+        dict: A dictionary representation of the CSV data.
+    """
+    # Load the data
     data = pd.read_csv(DATA_DIR / "data.csv")
     return data.to_dict()
 
 
 @app.post("/data")
 async def add_data(body: DataUpload):
+    """Add a random number to the CSV file.
+
+    Args:
+        body (DataUpload): The data model containing the random number.
+
+    Returns:
+        dict: A message indicating the random number was added.
+    """
+    # Load the data
     data = pd.read_csv(DATA_DIR / "data.csv")
+
+    # Add the new random number
     data = pd.concat(
         [data, pd.DataFrame({"RandomNumbers": [body.random_number]})], ignore_index=True
     )
+
+    # Save the data
     data.to_csv(DATA_DIR / "data.csv", index=False)
+
     return {"message": f"Random number {body.random_number} added to the data!"}
